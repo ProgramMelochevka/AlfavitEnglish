a4f2f75c69dc8aae120394ee350ea68357052123
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-09-30 23:07:30 +0300

    Вывод английского алфавита в консоль

---
+++ src/by/home/maxzzzit/AlfavitEnglish.java
@@ -0,0 +1,11 @@
+package by.home.maxzzzit;
+
+public class AlfavitEnglish {
+	public static void main (String[]args) {
+		char bukvi = 'A';
+		for (int i = 1; i <= 26; i++) {
+			System.out.print(bukvi+" ");
+			bukvi++;
+		}
+	}
+}
